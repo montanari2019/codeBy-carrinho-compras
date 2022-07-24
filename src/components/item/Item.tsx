af0706f38281bfc563e07e36dac48ca8842385a0
montanari2019 <ikaro.montanari31@gmail.com>
2022-07-23 21:17:10 -0400

    shoppingTask quase completo para receber renderização dinâmica

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,7 +1,14 @@
-export function Item(){
-    return (
-        <div>
-            <h1>Item</h1>
-        </div>
-    )
+import styleItem from "./Item.module.css";
+
+export function Item() {
+  return (
+    <div className={styleItem.container}>
+      <img src="https://images.unsplash.com/photo-1553452118-621e1f860f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+      <div className={styleItem.dataItem}>
+        <strong>Trufa de morango</strong>
+        <p>R$ 1,23</p>
+        <h6>R$ 1,21</h6>
+      </div>
+    </div>
+  );
 }
--- src/scenes/shopping/ShoppingCart.tsx
+++ src/scenes/shopping/ShoppingCart.tsx
@@ -1,25 +1,49 @@
-import useSWR from "swr"
+import useSWR from "swr";
+import styleShoppingCart from "./ShoppingCart.module.css";
+
 import { defaultFetcher } from "../../config/fetcher";
 import { IProductCart } from "../../model/IProductCart";
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 import { Header } from "../../components/header/Header";
+import { Item } from "../../components/item/Item";
+import { Badge } from "../../components/badge/Badge";
 
 export function ShoppingCart() {
-    const {selectedCard} = useParams()
-    const {data, error, isValidating} = useSWR<IProductCart>(selectedCard, defaultFetcher);
+  const { selectedCard } = useParams();
+  const { data, error, isValidating } = useSWR<IProductCart>(
+    selectedCard,
+    defaultFetcher
+  );
 
-    if(!!error){
-        return <h1>Deu erro</h1>
-    }
+  if (!!error) {
+    return <h1>Deu erro</h1>;
+  }
 
-    if(!data || isValidating) {
-        <h1>Carregando...</h1>
-    }
+  if (!data || isValidating) {
+    <h1>Carregando...</h1>;
+  }
 
-    return (
+  return (
+    <div>
+      <Header />
+      <div className={styleShoppingCart.container}>
+        <strong>Meu carrinho</strong>
         <div>
-            <Header/>
-            {JSON.stringify(data)}
+          <div className={styleShoppingCart.flexContainerListItem}>
+            <Item />
+            <Item />
+            <Item />
+          </div>
+          <div className={styleShoppingCart.totalizers}>
+            <div className={styleShoppingCart.totalizersItem}>
+              <strong>Total</strong>
+              <strong>R$ 9,50</strong>
+            </div>
+            { selectedCard === "acima-10" ? <Badge/> : <br/>}
+            
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
