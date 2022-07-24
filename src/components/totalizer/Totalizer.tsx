750acfaf3a63c04e8383a42158c7f80b253c80f8
montanari2019 <ikaro.montanari31@gmail.com>
2022-07-24 14:01:02 -0400

    configurando para deploy no heroku

--- src/components/totalizer/Totalizer.tsx
+++ src/components/totalizer/Totalizer.tsx
@@ -4,6 +4,7 @@ import styleTotalizer from "./Totalizer.module.css";
 export function Totalizer({ id, name, value }: TotalizerProduct) {
 
   const valueConverted = (value/100).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+  
   return (
     <div className={styleTotalizer.totalizersItem}>
       <strong>Total</strong>
