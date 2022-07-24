779caa856f63c86e31d0cecffeedccb5bc0374a6
montanari2019 <ikaro.montanari31@gmail.com>
2022-07-24 14:42:25 -0400

    tentativa 05

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ import express from 'express'
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url';
 
+
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use('/', express.static(resolve(__dirname, './dist')))
+app.use('/', express.static(path.join(__dirname, './dist')))
 
 app.listen(process.env.PORT || 3000, (erro) =>{
     if(erro){
